315ccd4c4050e9c824411f340a62f07953fc09c1
Medusa-85 <lima.mariana012@gmail.com>
2023-01-17 11:15:48 -0300

    complemento

--- src/index.ts
+++ src/index.ts
@@ -25,14 +25,38 @@ app.listen(3003, () => {
 })
 
 app.get("/users", (req: Request, res: Response) => {
-    res.status(200).send(getUsers());
+    res.status(200).send(users);
 })
 
 app.get("/products", (req: Request, res: Response) => {
-    res.status(200).send(getAllProducts())
+    res.status(200).send(products)
 })
 
 app.get("/products/search", (req: Request, res: Response) => {
     const q = req.query.q
     res.status(200).send(getProductByName("Caminha"))
 })
+
+app.post("/users", (req: Request, res: Response) => {
+    const id = req.body.id
+    const email = req.body.email
+    const password = req.body.password
+    res.status(201).send("Cadastro realizado com sucesso")
+})
+
+app.post("/products", (req: Request, res: Response) => {
+    const id = req.body.id
+    const name = req.body.name
+    const price = req.body.price
+    const category = req.body.category
+    res.status(201).send("Produto cadastrado com sucesso")
+})
+
+app.post("/purchases", (req: Request, res: Response) => {
+    const userId = req.body.userId
+    const productId = req.body.productId
+    const quantity = req.body.quantity
+    const totalPrice = req.body.totalPrice
+    res.status(201).send("Compra realizada com sucesso")
+})
+
